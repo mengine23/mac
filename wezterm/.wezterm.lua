a2838ab9dd0431ddcf7d712e30ef0348496077a2
Christopher Müller <cm@mindengine.de>
2024-09-28 09:05:38 +0200

    added wezterm config file

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,29 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+config = {
+    window_background_opacity = 0.8,
+    macos_window_background_blur = 10,
+    window_decorations = "RESIZE",
+    font = wezterm.font("MesloLGS Nerd Font Mono"),
+    font_size = 16,
+
+    colors = {
+        foreground = "#CBE0F0",
+        background = "#011423",
+        cursor_bg = "#47FF9C",
+        cursor_border = "#47FF9C",
+        cursor_fg = "#011423",
+        selection_bg = "#033259",
+        selection_fg = "#CBE0F0",
+        ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+        brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+    }
+}
+
+-- and finally, return the configuration to wezterm
+return config
